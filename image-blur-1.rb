ee9eef20f85300cdddc22b51d9bfc34cf380268c
CCrowRun <carrie.g.crow@gmail.com>
2019-08-28 20:58:29 -0500

    Implemented Image Blur #2

--- image-blur-1.rb
+++ image-blur-1.rb
@@ -13,4 +13,20 @@ class Image
       print "\n"
     end
   end
+
+  def blur_image
+    blur = Array.new(5) { Array.new(4, 0) }
+    image.each_with_index do |row, row_index|
+      row.each_with_index do |col, col_index|
+        if col == 1
+          blur[row_index][col_index] = 1
+          blur[row_index-1][col_index] = 1 if row_index - 1 >= 0 
+          blur[row_index+1][col_index] = 1 if row_index + 1 < image.length
+          blur[row_index][col_index-1] = 1 if col_index - 1 >= 0
+          blur[row_index][col_index+1] = 1 if col_index + 1 < row.length
+        end
+      end
+    end
+    return blur
+  end
 end
