026f60b3ae97cd3bf0ff469a62f7fe78e186ddaa
CCrowRun <carrie.g.crow@gmail.com>
2019-09-02 14:18:58 -0500

    Image Blur 3
    Finished Manhattan Distance Logic iteratively

--- image-blur-1.rb
+++ image-blur-1.rb
@@ -29,4 +29,43 @@ class Image
     end
     return blur
   end
+
+  def blur(distance = 1)
+    blur = Array.new(image.length) { Array.new(image[0].length, 0) }
+    image.each_with_index do |row, row_index|
+      row.each_with_index do |col, col_index|
+        if col == 1
+          row_min = row_index - distance < 0 ? 0 : row_index - distance 
+          row_max = row_index + distance > image.length - 1 ? image.length - 1 : row_index + distance 
+          row_current = row_index
+          counter = 0
+          while row_current >= row_min
+            col_min = col_index - distance + counter < 0 ? 0 : col_index - distance + counter
+            col_max = col_index + distance - counter > row.length - 1 ? row.length - 1 : col_index + distance - counter
+            col_current = col_min
+            while col_current <= col_max
+              blur[row_current][col_current] = 1
+              col_current += 1
+            end
+            row_current -= 1
+            counter += 1
+          end
+          row_current = row_index
+          counter = 0
+          while row_current <= row_max
+            col_min = col_index - distance + counter < 0 ? 0 : col_index - distance + counter
+            col_max = col_index + distance - counter > row.length - 1 ? row.length - 1 : col_index + distance - counter
+            col_current = col_min
+            while col_current <= col_max
+              blur[row_current][col_current] = 1
+              col_current += 1
+            end
+            row_current += 1
+            counter += 1
+          end 
+        end
+      end
+    end
+    return blur
+  end
 end
